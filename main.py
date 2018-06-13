5aeef618a25b5a3a21c3ea449000bf515f295b68
Михайлов Михаил <mihdevel@gmail.com>
2018-06-13 08:38:18 +0300

    Four task 'Json in HTML'

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import json
 
+# КОММЕНТАРИЙ
+# Вроде сделал правильно, но оно не работает, может что-то не понимаю.
+# Долго сидел дебажил на pycharm-e, но переменная html сбрасывается в какой-то момент
+
 # Получение json из файла
 with open('source.json') as json_file:
 	json = json.load(json_file)
+
 
 # Check типа и добавление тегов списка
 def CheckTrigerAndAddTeg(type_is_list, teg):
@@ -10,10 +15,10 @@ def CheckTrigerAndAddTeg(type_is_list, teg):
 		return teg
 	else:
 		return ''
+	
 
-
-def Main(json):
-	html = ''
+# Рекурсия в функции для обхода всех элементов
+def iteracions(json, html):
 	
 	# Проверка на тип list и сохранение тригера
 	if type(json) is list:
@@ -22,22 +27,32 @@ def Main(json):
 		type_is_list = 0
 	
 	
-	html += CheckTrigerAndAddTeg(type_is_list, '<ul>')
+	for object in json:
+		
+		html += CheckTrigerAndAddTeg(type_is_list, '<ul>')
+		
+		# Если переменная object это список или словарь, то функция вызывает саму себя
+		if type(object) is list or type(object) is dict:
+			iteracions(object, html)
+			
+		# Иначе строится переменная html
+		else:
+			html += CheckTrigerAndAddTeg(type_is_list, '<li>')
+			html += '<' + object + '>' + json[object] + '</' + object + '>'
+			html += CheckTrigerAndAddTeg(type_is_list, '</li>')
+
+		html += CheckTrigerAndAddTeg(type_is_list, '</ul>')
+		type_is_list = 0
+
+	return html
+	
+
+def Main(json):
+	html = ''
 	
 	# Проход по всему списку
-	for section in json:
-		
-		html += CheckTrigerAndAddTeg(type_is_list, '<li>')
-		
-		for key in section:
-			html += '<'+key+'>' + section[key] + '</'+key+'>'
-		
-		html += CheckTrigerAndAddTeg(type_is_list, '</li>')
+	html = iteracions(json, html)
 	
-	
-	html += CheckTrigerAndAddTeg(type_is_list, '</ul>')
-
-
 	return html
 
 
